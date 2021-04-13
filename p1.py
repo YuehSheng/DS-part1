676a76e8d58cdcccebd8f5c2400878ee6e8cf0fd
Godkiller12345 <jayanmri7@gmail.com>
2021-04-13 20:42:26 +0800

    合併

--- p1.py
+++ p1.py
@@ -7,10 +7,7 @@ plt.rcParams['font.size'] = 9
 
 data = pd.read_csv('data.csv', encoding='big5') 
 
-data = data.sort_values(by='總人口數')
 people = {'總人口數': data['總人口數']} 
-
-print(data['總人口數'].sum())
 
 # 將 <2% 歸類到其他
 threshold = data[data['總人口數'] > data['總人口數'].sum()/100*2]
@@ -25,11 +22,34 @@ plt.pie( threshold['總人口數'] ,
     autopct='%1.1f%%'
     ) 
 
-plt.title("109年11月份金門縣村里人口數",
+plt.title("110年3月份金門縣村里人口數",
     fontdict = {'fontsize': 12,
     'fontweight' : 'bold',
     'horizontalalignment': 'center'}, x=0.5, y=1)
 plt.axis('equal')
-plt.savefig('Population.png') 
-plt.show()
+plt.savefig('Population.png')
 
+
+move = data[['遷出人數', '遷入人數']]
+move.index = data['村別數']
+move.plot(kind='bar', stacked=True)
+
+
+plt.title("110年3月份金門縣村里遷出入人口數")
+plt.savefig('PopulationDiff.png')
+
+
+gender = data[['出生人數', '死亡人數']]
+gender.index = data['村別數']
+gender.plot.bar(figsize=(20, 5))
+
+plt.title("110年3月份金門縣村里生死長條圖")
+plt.savefig('PopulationBD.png')
+
+
+gender = data[['男數', '女數']]
+gender.index = data['村別數']
+gender.plot.bar(subplots=True)
+
+plt.title("110年3月份金門縣村里男女長條圖")
+plt.savefig('PopulationGender.png')
